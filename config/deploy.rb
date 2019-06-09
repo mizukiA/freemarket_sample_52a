2ce0231781a7e422fee0e6f0cb03475f30d894e5
Mizuki Ando <mzk5326@gmail.com>
2019-06-09 17:35:30 +0900

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, 'freemarket_sample_52a'
+set :repo_url,  'git@github.com:mizukiA/freemarket_sample_52a.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/freemarket.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
