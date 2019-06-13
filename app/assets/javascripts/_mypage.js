6ff021c8145075f45c9c9200dfb4d3af0607e53c
daigo-fujioka <work.daigo@gmail.com>
2019-06-13 21:16:08 +0900

    Summry(required)

---
+++ app/assets/javascripts/_mypage.js
@@ -0,0 +1,42 @@
+$(function(){
+    $('.a').on('click',function(){
+        $(this).addClass('active')
+        $('.b').removeClass('active')
+        $('#user-main__content-todo').css({
+            "display": "block"
+        });
+        $('#user-main__content-notification').css({
+            "display": "none"
+        })
+    });
+    $('.b').on('click',function(){
+        $(this).addClass('active')
+        $('.a').removeClass('active')
+        $('#user-main__content-todo').css({
+            "display": "none"
+        });
+        $('#user-main__content-notification').css({
+            "display": "block"
+        })
+    });
+    $('.c').on('click',function(){
+        $(this).addClass('active')
+        $('.d').removeClass('active')
+        $('#user-main__content-deal').css({
+            "display": "block"
+        })
+        $('#user-main__content-old').css({
+            "display": "none"
+        })
+    });
+    $('.d').on('click',function(){
+        $(this).addClass('active')
+        $('.c').removeClass('active')
+        $('#user-main__content-deal').css({
+            "display": "none"
+        })
+        $('#user-main__content-old').css({
+            "display": "block"
+        })
+    });
+})
