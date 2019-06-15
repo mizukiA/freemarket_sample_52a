20f95c3c9f3020f1375770dcde0ccb2ca703e88c
create <silence.24yue@gmail.com>
2019-06-15 13:52:31 +0900

    Profile Markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/logout' => 'users#logout'
   get 'users/signup' => 'users#signup'
+  get 'users/profile' => 'users#profile'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
