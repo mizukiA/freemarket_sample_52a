6a8e1173c671358cbe3353364f663ae4c45f88fd
daigo-fujioka <work.daigo@gmail.com>
2019-06-17 16:38:37 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :cards
   devise_for :addresses
   devise_for :profiles
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items do
