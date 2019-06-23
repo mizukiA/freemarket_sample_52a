5c2eda7b1f732d44a27013222f7083d239fe5d81
Mizuki Ando <mzk5326@gmail.com>
2019-06-23 13:46:44 +0900

    add basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -19,4 +20,17 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path # サインアウト後のリダイレクト先URL
   end
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
