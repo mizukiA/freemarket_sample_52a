efef44a209f36c7e3e7b043d4a366b87e8e482ed
create <silence.24yue@gmail.com>
2019-06-30 02:27:17 +0900

    Minor adjustment

--- app/assets/javascripts/_sign.js
+++ app/assets/javascripts/_sign.js
@@ -2,5 +2,7 @@ $(function(){
   $(window).load(function () {
     $(".error:contains('blank')").css('display', 'none');
     $(".error:contains('short')").css('display', 'none');
+    $(".error:contains('match')").css('display', 'none');
+    $(".error:contains('already')").css('display', 'none');
   });
 });
