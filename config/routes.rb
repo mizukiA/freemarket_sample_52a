ea1a9887f5d0139b970edf8f2b1402527afd0a80
daigo-fujioka <work.daigo@gmail.com>
2019-06-30 19:47:05 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items do
-    collection do
+    member do
       get 'buy'
     end
     resources :comments, only: [:create,:destroy]
