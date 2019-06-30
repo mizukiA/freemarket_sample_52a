aeb2a4ade15f161a50403d97c7ae1ca0a678445e
Mizuki Ando <mzk5326@gmail.com>
2019-06-30 15:08:50 +0900

    delete env

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,9 +40,5 @@ end
 
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
-  username: ENV["BASIC_AUTH_USER"],
-  password: ENV["BASIC_AUTH_PASSWORD"]
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
 }
