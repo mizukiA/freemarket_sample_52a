8c9dc8bde8a3d962081848a48f3f5d215fc68c0d
create <silence.24yue@gmail.com>
2019-07-02 18:24:56 +0900

    bichousei

--- app/assets/javascripts/_sign.js
+++ app/assets/javascripts/_sign.js
@@ -1,8 +1,46 @@
 $(function(){
   $(window).load(function () {
-    $(".error:contains('blank')").css('display', 'none');
-    $(".error:contains('short')").css('display', 'none');
-    $(".error:contains('match')").css('display', 'none');
-    $(".error:contains('already')").css('display', 'none');
+    var txt = 'を入力してください';
+    var elem = null;
+    $('.error:contains('+txt+')').each(function(){
+        if($(this).text() == txt){
+          elem = $(this);
+          elem.css('display', 'none');
+        }
+    });
+    // $(".error:contains('blank')").css('display', 'none');
+  });
+  $(window).load(function () {
+    var txt = 'はすでに存在します';
+    var elem = null;
+    $('.error:contains('+txt+')').each(function(){
+        if($(this).text() == txt){
+          elem = $(this);
+          elem.css('display', 'none');
+        }
+    });
+    // $(".error:contains('already')").css('display', 'none');
+  });
+  $(window).load(function () {
+    var txt = 'とPasswordの入力が一致しません';
+    var elem = null;
+    $('.error:contains('+txt+')').each(function(){
+        if($(this).text() == txt){
+          elem = $(this);
+          elem.css('display', 'none');
+        }
+    });
+    // $(".error:contains('match')").css('display', 'none');
+  });
+  $(window).load(function () {
+    var txt = 'は6文字以上で入力してください';
+    var elem = null;
+    $('.error:contains('+txt+')').each(function(){
+        if($(this).text() == txt){
+          elem = $(this);
+          elem.css('display', 'none');
+        }
+    });
+    // $(".error:contains('short')").css('display', 'none');
   });
 });
